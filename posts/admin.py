8246e0e7071ece7a60090039ab431288c9832038
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-09-22 17:15:24 +0600

    post

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Thread
+from .models import Thread, Post
 
 class ThreadAdmin(admin.ModelAdmin):
     list_display = ["title", "slug", "created_at", "updated_at"]
@@ -9,3 +9,10 @@ class ThreadAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Thread, ThreadAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug","user","thread", "created_at"]
+
+    class Meta:
+        model = Post
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,7 @@ from bbs.helpers import get_dynamic_fields
 from bbs.utils import (
     unique_slug_generator
 )
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save, pre_save
 
 
@@ -45,6 +46,47 @@ class Thread(models.Model):
 
 
 # # -------------------------------------------------------------------
+# #                             Post
+# # -------------------------------------------------------------------
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=255
+    )
+    slug = models.SlugField(
+        unique=True
+    )
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="post_users"
+    )
+    thread = models.ForeignKey(
+        Thread, on_delete=models.CASCADE, related_name="post_threads"
+    )
+    weight = models.PositiveIntegerField(
+        default=0, blank=True, null=True, verbose_name="post weight"
+    )
+    description = models.TextField(
+        blank=True, null=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='created at'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name='updated at'
+    )
+
+    class Meta:
+        verbose_name = ("Post")
+        verbose_name_plural = ("Posts")
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
+
+    def get_fields(self):
+        return [get_dynamic_fields(field, self) for field in self.__class__._meta.fields]
+
+# # -------------------------------------------------------------------
 # #                  Pre-Save Post-Save Configurations
 # # -------------------------------------------------------------------
 
@@ -57,3 +99,14 @@ def thread_slug_pre_save_receiver(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(thread_slug_pre_save_receiver, sender=Thread)
+
+
+
+# # Post
+
+def thread_slug_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance=instance, field=instance.title)
+
+
+pre_save.connect(thread_slug_pre_save_receiver, sender=Post)
